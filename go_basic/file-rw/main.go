ceaf26af281f0b28387a566b610c84c0224f1998
litianwei <3245849061@qq.com>
2023-05-12 21:59:15 +0800

    go 特性读取文件的三种方式

--- go_basic/file-rw/main.go
+++ go_basic/file-rw/main.go
@@ -5,7 +5,10 @@ import (
 )
 
 func main() {
-	_case.CopyDirToDir()
-	_case.ReadWriteFiles()
-	_case.OneSideReadWriteToDest()
+	//_case.CopyDirToDir()
+	//_case.ReadWriteFiles()
+	//_case.OneSideReadWriteToDest()
+	_case.ReadLine1()
+	_case.ReadLine2()
+	_case.ReadLine3()
 }
